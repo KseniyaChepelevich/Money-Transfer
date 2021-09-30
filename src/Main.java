02131c0dcc3fc59722a09a2d7dca79ddd295f51f
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2021-09-30 19:02:01 +0300

    Money transfer

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        int account_balance = 2_000_000_000;
+        int transfer_amount = 500_000_000;
+        int total = account_balance + transfer_amount;
+        System.out.println(total);
     }
 }
+
